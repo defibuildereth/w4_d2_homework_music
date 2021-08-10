1ca8076da9c7c4f2b46604e7b31896b2142927f4
d7o1d1s0 <d7sarmy@gmail.com>
2021-08-10 21:14:30 +0100

    created and linked tables

---
+++ music_library_start/console.py
@@ -0,0 +1,19 @@
+import pdb
+from models.artist import Artist
+from models.album import Album
+
+import repositories.artist_repository as artist_repository
+import repositories.album_repository as album_repository
+
+album_repository.delete_all()
+artist_repository.delete_all()
+
+
+
+
+for album in album_repository.select_all():
+    print(album.__dict__)
+
+
+
+pdb.set_trace()
