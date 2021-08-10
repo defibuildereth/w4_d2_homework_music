a95ea2fd96087cb067e2be4f7361ab0afbb5f7b0
d7o1d1s0 <d7sarmy@gmail.com>
2021-08-10 21:53:01 +0100

    bug fixes

--- music_library_start/repositories/album_repository.py
+++ music_library_start/repositories/album_repository.py
@@ -10,9 +10,9 @@ def save(album):
     sql = "INSERT INTO albums (title, genre, artist_id) VALUES (%s, %s, %s) RETURNING *"
     values = [album.title, album.genre, album.artist.id]
     results = run_sql(sql, values)
-    # id = results[0]
-    # album.id = id
-    return results
+    id = results[0]['id']
+    album.id = id
+    return album
 
 def delete_all():
     sql = "DELETE FROM albums"
@@ -20,7 +20,13 @@ def delete_all():
 
 
 def select(id):
-    pass
+    album = None
+    sql = "SELECT * FROM albums WHERE id = %s"
+    values = [id]
+    results = run_sql(sql, values)
+    for row in results:
+        album = Album(row['title'], row['genre'], row['artist_id'], row['id'])
+    return album
 
 
 def delete(id):
